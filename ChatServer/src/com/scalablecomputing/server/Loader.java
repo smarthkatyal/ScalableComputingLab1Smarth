dd07ded3a2d9034fd75b51097c44fb511a3f288a
Smarth Katyal <Smarth Katyal@10.6.55.235>
2017-10-15 17:05:32 +0100

    FirstCommit

---
+++ ChatServer/src/com/scalablecomputing/server/Loader.java
@@ -0,0 +1,10 @@
+package com.scalablecomputing.server;
+
+public class Loader {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello");
+	}
+
+}
